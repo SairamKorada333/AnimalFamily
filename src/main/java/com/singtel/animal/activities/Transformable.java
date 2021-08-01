dcae4bdc757bd30d8c2a06ffb1d86feebf85b5be
sairamkorada333 <sairamkorada.java@gmail.com>
2021-08-01 21:51:01 +0800

    Add Transformable, Butterfly and Caterpiller classes

---
+++ src/main/java/com/singtel/animal/activities/Transformable.java
@@ -0,0 +1,8 @@
+package com.singtel.animal.activities;
+
+
+import com.singtel.animal.model.Animal;
+
+public interface Transformable extends AnimalAbility {
+    Animal transform();
+}
---
+++ src/main/java/com/singtel/animal/model/Butterfly.java
@@ -0,0 +1,7 @@
+package com.singtel.animal.model;
+
+
+import com.singtel.animal.activities.FlyAbility;
+
+public class Butterfly implements Animal, FlyAbility {
+}
